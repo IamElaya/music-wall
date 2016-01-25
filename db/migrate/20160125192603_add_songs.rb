4215e998a87d6ac6457f4a0f328b08286594fd17
IamElaya <asison13@gmail.com>
2016-01-25 19:55:51 +0000

    first commit

---
+++ db/migrate/20160125192603_add_songs.rb
@@ -0,0 +1,13 @@
+class AddSongs < ActiveRecord::Migration
+  def change
+
+    create_table :songs do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+
+  end
+end
