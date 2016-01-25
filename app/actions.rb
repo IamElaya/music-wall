1593a61defdfeb9c0e169fb0bbaace7777d3b8fb
IamElaya <asison13@gmail.com>
2016-01-25 20:21:20 +0000

    2nd commit

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,22 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @songs = Song.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+  @songs = Song.new(
+    title: params[:title],
+    url: params[:url],
+    author: params[:author]
+    )
+  @songs.save
+  redirect '/songs'
+end
